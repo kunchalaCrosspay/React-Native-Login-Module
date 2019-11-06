b9a90ed1a3aeaede29b61a713fe08924b9abf2d5
Sai kiran Kunchala <kunchalasai@gmail.com>
2019-11-06 10:48:34 +0530

    LoginModule commit

---
+++ src/store/actions/auth.js
@@ -0,0 +1,42 @@
+import { TRY_AUTH } from "./actionTypes";
+import { uiStartLoading, uiStopLoading } from "./index";
+import startMainTabs from "../../screens/MainTabs/startMainTabs";
+
+export const tryAuth = (authData, authMode) => {
+  return dispatch => {
+    dispatch(uiStartLoading());
+    const apiKey = "AIzaSyCnX8rTPN4YtEZiX5FMYkqQtXJNLu80GPU";
+    let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + apiKey;
+    if (authMode === "signup") {
+        url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + apiKey
+    }
+    fetch(
+        url,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: authData.email,
+            password: authData.password,
+            returnSecureToken: true
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      )
+        .catch(err => {
+          console.log(err);
+          alert("Authentication failed, please try again!");
+          dispatch(uiStopLoading());
+        })
+        .then(res => res.json())
+        .then(parsedRes => {
+          dispatch(uiStopLoading());
+          if (parsedRes.error) {
+            alert("Authentication failed, please try again!");
+          } else {
+            startMainTabs();
+          }
+        });
+  };
+};
